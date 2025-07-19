7c422aa5a2bb9973c1f099e656795574f15799bc
shivsunder140 <73846640+shivsunder140@users.noreply.github.com>
2025-07-20 00:13:28 +0530

    Create missing ContentCalendar page component

---
+++ src/pages/content/ContentCalendar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContentCalendar from '../../components/content/ContentCalendar';
+
+const ContentCalendarPage: React.FC = () => {
+  return <ContentCalendar />;
+};
+
+export default ContentCalendarPage;
